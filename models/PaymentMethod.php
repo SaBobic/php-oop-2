a82b830fd7174db44faac1e85f187241095e742a
SaBobic <fabrizioettori@gmail.com>
2022-09-12 16:32:49 +0200

    added PaymentMethod class

---
+++ models/PaymentMethod.php
@@ -0,0 +1,53 @@
+<?php
+
+class PaymentMethod
+{
+    private $balance;
+    private $expiration_date;
+
+    public function __construct($balance, $expiration_date)
+    {
+        $this->setBalance($balance);
+        $this->setExpirationDate($expiration_date);
+    }
+
+    /**
+     * Get the value of balance
+     */ 
+    public function getBalance()
+    {
+        return $this->balance;
+    }
+
+    /**
+     * Set the value of balance
+     *
+     * @return  self
+     */ 
+    public function setBalance($balance)
+    {
+        $this->balance = $balance;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of expiration_date
+     */ 
+    public function getExpirationDate()
+    {
+        return $this->expiration_date;
+    }
+
+    /**
+     * Set the value of expiration_date
+     *
+     * @return  self
+     */ 
+    public function setExpirationDate($expiration_date)
+    {
+        $this->expiration_date = $expiration_date;
+
+        return $this;
+    }
+}
